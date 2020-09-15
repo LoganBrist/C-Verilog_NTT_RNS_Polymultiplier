e6dfc57ad21c1c0f4161e077122c2e9d3074f51e
LoganBrist <44846549+LoganBrist@users.noreply.github.com>
2020-09-15 12:21:55 -0700

    Update MAC.v

--- Polymult_hardware.srcs/sources_1/new/MAC.v
+++ Polymult_hardware.srcs/sources_1/new/MAC.v
@@ -25,33 +25,38 @@ module RNS_MAC
     parameter CH_BW      = 32,
     parameter N_CHANNELS = 4,
     parameter RNS_BW     = CH_BW * N_CHANNELS,                 //total RNS buswidth 
-    parameter RNS        = {32'd4294967291, 32'd4294967279, 32'd4294967231, 32'd4294967197}
+    parameter MOD        = 32'd4294967291
     )
     (
     input  wire CLK,
     input  wire [RNS_BW-1:0]  A,
     input  wire [RNS_BW-1:0]  B,
-    input  wire reset,
-    output wire  [RNS_BW-1:0]  Z
+    output wire  [CH_BW-1:0]  Z
     );
     
-    /*
-    wire [RNS_BW-1:0] m_result; 
-    RNS_MULT #(CH_BW, N_CHANNELS, RNS_BW, RNS) m2(A, B, m_result); 
-    RNS_ADD #(CH_BW, N_CHANNELS, RNS_BW, RNS) a1(m_result, m_result, Z);
-    */
-    
-// Parallel generation
-    
-    genvar i;
-    generate
-        for (i = 0; i < N_CHANNELS; i = i + 1) begin
-            integer L = CH_BW*i;
-             MOD_MAC#(CH_BW, RNS[L+:CH_BW]) mac(CLK,A[L+:CH_BW],B[L+:CH_BW], reset, Z[L+:CH_BW]);
-        end
-    endgenerate
-    
-    // end parallel generation
-                   
-endmodule
 
+             //repeat this loop j times for different Mi_j
+   //multiply   
+    genvar i;
+    wire [RNS_BW-1:0] int_val;  
+     generate
+         for (i = 0; i < N_CHANNELS; i = i + 1) begin
+             integer L = CH_BW*i;                                                          //for each i channel, multiply sigma_i times M_i (jth M_i) and sum
+             assign int_val[CH_BW*i +:CH_BW] = (A[L+:CH_BW] * B[L+:CH_BW]) % MOD;        
+          end
+     endgenerate
+
+
+//accumulate
+    reg [CH_BW+N_CHANNELS-1:0] sum;
+    integer idx;
+    always @* begin
+           sum = {CH_BW{1'b0}};
+            for( idx = 0; idx < N_CHANNELS; idx = idx + 1) begin
+              sum = sum + int_val[CH_BW*idx +:CH_BW];
+            end
+     end
+           
+     assign Z = sum % MOD;
+     
+endmodule
